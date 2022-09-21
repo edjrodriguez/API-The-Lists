75b36210b6ebb1718a58340149a5df4e2aebbd0a
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-09-21 12:13:42 -0600

    setup get functionality

---
+++ lists.js
@@ -0,0 +1,76 @@
+const lists = {
+    groceries: [
+        {
+            id: 1,
+            item: "green apples",
+            quantity: "10",
+            description: ""
+        }, 
+        {
+            id: 2,
+            item: "bell peppers",
+            quantity: "2",
+            description: "1 red and 1 striped"
+        },
+        {
+            id: 3,
+            item: "onion",
+            quantity: "1",
+            description: "NOT Organic"
+        },
+        {
+            id: 4,
+            item: "Siggi's Yogurt",
+            quantity: "5",
+            description: "2 Raspberry and 3 Mixed berry"
+        },
+        {
+            id: 5,
+            item: "Dave's Killer Bread",
+            quantity: "1",
+            description: "21 Whole Grains"
+        },
+        {
+            id: 6,
+            item: "Coffee",
+            quantity: "1",
+            description: "Whole bean NOT ground"
+        }
+    ],
+    whitWishList: [
+        {
+            id: 1,
+            item: "Bike Lights",
+            quantity: "1",
+            description: "https://www.amazon.com/dp/B08LGLF969/ref=cm_sw_r_api_i_P77G55N2JFG7JCGPCT3Y_0"
+        },
+        {
+            id: 2,
+            item: "Bombas socks",
+            quantity: "2 pair",
+            description: "low cut"
+        },
+        {
+            id: 3,
+            item: "Snow gloves",
+            quantity: "1 pair",
+            description: "warm and waterproof"
+        }
+    ],
+    eddieWishList:[
+        {
+            id: 1,
+            item: "Rollerblades",
+            quantity: "1",
+            description: "https://www.amazon.com/dp/B09KC3LHYN/ref=cm_sw_r_api_i_1T25820AGZN9YRVKHF12_0?_encoding=UTF8&psc=1"
+        },
+        {
+            id: 2,
+            item: "Melodica",
+            quantity: "1",
+            description: "https://www.amazon.com/dp/B000XYFBMK/ref=cm_sw_r_api_i_46PPMTMC10ZTVH1KZWSQ_0?_encoding=UTF8&psc=1"
+        }
+    ] 
+}
+
+module.exports = lists
--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+const lists = require('./lists');
+const { response } = require('express');
 
-app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(cors());
+
+app.locals.lists = lists
+
+app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Wish List API';
+
+app.get('/', (request, response) => {
+    response.send('Wish List API');
+  });
+  
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+    console.log(lists)
+  });
+  
+app.get('/lists', (request, response) => {
+   return response.send(app.locals.lists)
+})
+  
