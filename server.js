d3a0d1cf22725743055f2d6a716ed761f89cef41
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-09-22 11:19:50 -0600

    Add code to allow posting of datato the lists

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const lists = require('./lists');
+let postID = 100
 // const { response } = require('express');
 
 app.use(express.json());
@@ -21,6 +22,58 @@ app.get('/', (request, response) => {
   });
   
 app.get('/lists', (request, response) => {
+  console.log(app.locals.lists)
    return response.send(app.locals.lists)
 })
   
+app.post('/lists', (request, response) => {
+  if(request.body.groceryItem) {
+    const {id, groceryItem, quantity, description} = request.body;
+
+    if (!id || !groceryItem || !quantity || !description) {
+      return response.status(422).json({
+        error: 'Expected format { id: <Number>, groceryItem: <String>, quantity: <String>, description: <String> }. You are missing a required parameter.'
+      })
+    }
+  
+    const newItem = {id: postID, groceryItem, quantity, description};
+    postID += 1
+  
+    app.locals.lists.groceries = [...app.locals.lists.groceries, newItem];
+  
+    return response.status(201).json(newItem)
+   
+  } else if(request.body.whitneyItem) {
+    const {id, whitneyItem, link, description} = request.body;
+
+    if (!id || !whitneyItem || !link || !description) {
+      return response.status(422).json({
+        error: 'Expected format { id: <Number>, whitneyItem: <String>, link: <String>, description: <String> }. You are missing a required parameter.'
+      })
+    }
+
+    const newItem = {id: postID, whitneyItem, link, description};
+    postID += 1
+
+    app.locals.lists.whitWishList = [...app.locals.lists.whitWishList, newItem];
+
+    return response.status(201).json(newItem)
+    
+  } else if(request.body.eddieItem) {
+    const {id, eddieItem, link, description} = request.body;
+
+    if (!id || !eddieItem || !link || !description) {
+      return response.status(422).json({
+        error: 'Expected format { id: <Number>, eddieItem: <String>, link: <String>, description: <String> }. You are missing a required parameter.'
+      })
+    }
+
+    const newItem = {id: postID, eddieItem, link, description};
+    postID += 1
+
+    app.locals.lists.eddieWishList = [...app.locals.lists.eddieWishList, newItem];
+
+    return response.status(201).json(newItem)
+    
+  }
+})
